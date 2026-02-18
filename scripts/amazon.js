9681dede9668f2e73e99b8c2634b10956baf0bbd
Maitai-developer <maitaicollins007@gmail.com>
2026-02-18 09:12:57 +0300

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -109,8 +109,13 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
     quantity:1
    });
   }
- 
-  console.log(cart)
+//   a variable to store total quantity
+let cartQuantity =0;
+   cart.forEach((item)=>{
+  cartQuantity += item.quantity;
+   });
+   document.querySelector('.js-cart-quantity').innerHTML=cartQuantity
+
 })
 })
  
