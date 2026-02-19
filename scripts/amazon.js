d9f1f56542ffeb9a45e44b969f2447afd8f12816
Maitai-developer <maitaicollins007@gmail.com>
2026-02-19 06:37:53 +0300

    Import products(variable) from product.js as a module

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 // This code will take the variable cart of cart.js and we use this file path to access it
 import {cart} from '../data/cart.js';
+import { products } from '../data/products.js';
 // const products =[{
 //     image:'images/products/athletic-cotton-socks-6-pairs.jpg',
 //     name :'Black and Gray Athletic Cotton Socks - 6 Pairs',
